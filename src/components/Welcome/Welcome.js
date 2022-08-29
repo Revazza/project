0601e9bcd047550fe5787f7ac8d227bb401dcd8c
Sandro Revazishvili <sandro.revazishviliii@gmail.com>
2022-08-29 15:33:27 +0400

    Change buttons to links

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,14 +1,18 @@
-import Button from "../../UI/Button/Button";
 import styles from "./Welcome.module.scss";
+import { Link } from "react-router-dom";
 
 function Welcome() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.logo}></div>
       <div className={styles.welcome_img}></div>
-      <div className={styles.btn_wrapper}>
-        <Button text="ჩანაწერის დამატება" />
-        <Button text="ჩანაწერის სია" />
+      <div className={styles.nav_wrapper}>
+        <div className={styles.nav}>
+          <Link to="/emp-info">ჩანაწერის დამატება</Link>
+        </div>
+        <div className={styles.nav}>
+          <Link to="/list">ჩანაწერის სია</Link>
+        </div>
       </div>
     </div>
   );
